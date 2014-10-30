d3828f8543251f82ddbf0e1154a9c669bcd75e74
Prateekshit <prateekshit12078@iiitd.ac.in>
2014-10-30 16:15:31 +0530

    Seam removal done... memory issues

--- m1.cpp
+++ m1.cpp
@@ -32,6 +32,8 @@ int		bestSeamX_helper (Mat &M, Mat &J, Mat &energy, int i, int j);
 seam 	bestSeamY (Mat energy);
 int		bestSeamY_helper (Mat &M, Mat &J, Mat &energy, int i, int j);
 int 	minNeighbor (int x1, int x2, int x3, int i, int j, Mat &J);
+void 	removeSeamY (Mat &img, Mat &newImg, seam &s);
+void 	removeSeamX (Mat &img, Mat &newImg, seam &s);
 
 int main (int argc, char **argv) {
 	int i, j, n;
@@ -41,31 +43,28 @@ int main (int argc, char **argv) {
 	
 	img = imread(argv[1]);
 	cvtColor(img, img, CV_RGB2GRAY);
-	// imshow("Original Image", img);
-	GaussianBlur(img, gaussImg, Size(3,3), 1);
-	// imshow("Gaussian New", gaussImg);
-	// waitKey(0);
-	// destroyWindow("Original Image");
-	// destroyWindow("Gaussian New");
-
-	Sobel(gaussImg, sobelx, CV_16S, 1, 0, 3, 1, 0, BORDER_DEFAULT);
-	convertScaleAbs(sobelx, sobelx);
-	Sobel(gaussImg, sobely, CV_16S, 0, 1, 3, 1, 0, BORDER_DEFAULT);
-	convertScaleAbs(sobely, sobely);
-	// imshow("Sobel x", sobelx);
-	// imshow("Sobel y", sobely);
-	// waitKey(0);
-	// destroyWindow("Sobel x");
-	// destroyWindow("Sobel y");
-
-	addWeighted(sobelx, 0.5, sobely, 0.5, 0, energy);
-	energy.convertTo(energy, CV_64FC1);
-	// imshow("Energy", energy);
-	// waitKey(0);
-	// destroyWindow("Energy");
+	imshow("Original Image", img);
 	Mat dummy = (Mat_<double>(5, 5) << 5.5, 16, 22.5, 13, 16, 6.5, 8, 11.5, 20, 6.5, 2.5, 9.5, 7, 7.5, 4, 5.5, 3.5, 5.5, 10, 4.5, 6.5, 3.5, 9.5, 9, 5.5);
-	struct seam SX = bestSeamY(dummy);
-	struct seam SY = bestSeamX(dummy);
+	
+	// struct seam SX = bestSeamX(energy);
+	Mat newImg;
+	for (int i=0; i<60; i++) {
+		GaussianBlur(img, gaussImg, Size(3,3), 1);
+		Sobel(gaussImg, sobelx, CV_16S, 1, 0, 3, 1, 0, BORDER_DEFAULT);
+		convertScaleAbs(sobelx, sobelx);
+		Sobel(gaussImg, sobely, CV_16S, 0, 1, 3, 1, 0, BORDER_DEFAULT);
+		convertScaleAbs(sobely, sobely);
+		addWeighted(sobelx, 0.5, sobely, 0.5, 0, energy);
+		energy.convertTo(energy, CV_64FC1);
+		struct seam SY = bestSeamY(energy);
+		newImg = Mat::zeros(img.rows, img.cols-1, CV_8UC1);
+		removeSeamY(img, newImg, SY);
+		img = newImg;
+	}
+	
+	imshow("Lol", img);
+	waitKey(0);
+	destroyAllWindows();
 	return 0;
 }
 
@@ -81,11 +80,6 @@ seam bestSeamY (Mat energy) {
 			index = i;
 		}
 	}
-
-	cout<<energy<<endl;
-	cout<<M<<endl;
-	cout<<J;
-	cout<<"\nMin "<<minimum<<"; minIndex "<<index<<endl;
 	struct seam S;
 	int colIndex = index;
 	// cout<<"Col Index "<<colIndex<<endl;
@@ -98,9 +92,6 @@ seam bestSeamY (Mat energy) {
 		struct pixel p = {(int)(i-1), (int)colIndex};
 		S.pixels.push_back(p);
 		S.totalEnergy += energy.at<double>(i-1, colIndex);
-	}
-	for (int i=0; i<energy.cols; i++) {
-		cout<<"x "<<S.pixels[i].x<<"; y "<<S.pixels[i].y<<endl;
 	}
 	return S;
 }
@@ -138,8 +129,17 @@ seam bestSeamX (Mat energy) {
 			index = i;
 		}
 	}
-	cout<<"\nMin "<<minimum<<"; minIndex "<<index<<endl;
 	struct seam S;
+	int rowIndex = index;
+	struct pixel p = {rowIndex, (energy.rows-1)};
+	S.pixels.push_back(p);	S.totalEnergy = energy.at<double>(rowIndex, energy.rows-1);
+	for (int i=energy.cols-1; i>0; i--) {
+		int prev = rowIndex;
+		rowIndex += (int)J.at<double>(prev, i);
+		struct pixel p = {rowIndex, i-1};
+		S.pixels.push_back(p);
+		S.totalEnergy += energy.at<double>(rowIndex, i-1);
+	}
 	return S;
 }
 
@@ -175,4 +175,31 @@ int minNeighbor (int x1, int x2, int x3, int i, int j, Mat &J) {
 	// cout<<x1<<" "<<x2<<" "<<x3<<" "<<minVal<<endl;
 	J.at<double>(i,j) = pos;
 	return minVal;
+}
+
+void removeSeamY (Mat &img, Mat &newImg, seam &s) {
+	
+	int initx, inity;
+	for (int i=0; i<s.pixels.size(); i++) {
+		initx = s.pixels[i].x;	inity = s.pixels[i].y;
+		for (int j=0; j<inity; j++) {
+			newImg.at<uchar>(initx, j) = img.at<uchar>(initx, j);
+		}
+		for (int j=inity+1; j<img.cols; j++) {
+			newImg.at<uchar>(initx, j-1) = img.at<uchar>(initx, j);
+		}
+	}
+}
+
+void removeSeamX (Mat &img, Mat &newImg, seam &s) {
+	int initx, inity;
+	for (int i=0; i<s.pixels.size(); i++) {
+		initx = s.pixels[i].x;	inity = s.pixels[i].y;
+		for (int j=0; j<initx; j++) {
+			newImg.at<uchar>(j, inity) = img.at<uchar>(j, inity);
+		}
+		for (int j=inity+1; j<img.cols; j++) {
+			newImg.at<uchar>(j-1, inity) = img.at<uchar>(j, inity);
+		}
+	}
 }
