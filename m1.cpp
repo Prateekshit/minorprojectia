316fb00f187248b02cffb582acd4f54394808f0d
Prateekshit <prateekshit12078@iiitd.ac.in>
2014-10-30 16:34:10 +0530

    Tried it, working on Lena!

--- m1.cpp
+++ m1.cpp
@@ -43,12 +43,15 @@ int main (int argc, char **argv) {
 	
 	img = imread(argv[1]);
 	cvtColor(img, img, CV_RGB2GRAY);
+	
 	imshow("Original Image", img);
-	Mat dummy = (Mat_<double>(5, 5) << 5.5, 16, 22.5, 13, 16, 6.5, 8, 11.5, 20, 6.5, 2.5, 9.5, 7, 7.5, 4, 5.5, 3.5, 5.5, 10, 4.5, 6.5, 3.5, 9.5, 9, 5.5);
+	resize(img, img, Size(img.rows*1.2, img.cols*1.2), 0, 0, INTER_CUBIC);
+	
+	// Mat dummy = (Mat_<double>(5, 5) << 5.5, 16, 22.5, 13, 16, 6.5, 8, 11.5, 20, 6.5, 2.5, 9.5, 7, 7.5, 4, 5.5, 3.5, 5.5, 10, 4.5, 6.5, 3.5, 9.5, 9, 5.5);
 	
 	// struct seam SX = bestSeamX(energy);
 	Mat newImg;
-	for (int i=0; i<60; i++) {
+	for (int i=0; i<50; i++) {
 		GaussianBlur(img, gaussImg, Size(3,3), 1);
 		Sobel(gaussImg, sobelx, CV_16S, 1, 0, 3, 1, 0, BORDER_DEFAULT);
 		convertScaleAbs(sobelx, sobelx);
@@ -61,7 +64,20 @@ int main (int argc, char **argv) {
 		removeSeamY(img, newImg, SY);
 		img = newImg;
 	}
-	
+
+	for (int i=0; i<50; i++) {
+		GaussianBlur(img, gaussImg, Size(3,3), 1);
+		Sobel(gaussImg, sobelx, CV_16S, 1, 0, 3, 1, 0, BORDER_DEFAULT);
+		convertScaleAbs(sobelx, sobelx);
+		Sobel(gaussImg, sobely, CV_16S, 0, 1, 3, 1, 0, BORDER_DEFAULT);
+		convertScaleAbs(sobely, sobely);
+		addWeighted(sobelx, 0.5, sobely, 0.5, 0, energy);
+		energy.convertTo(energy, CV_64FC1);
+		struct seam SX = bestSeamX(energy);
+		newImg = Mat::zeros(img.rows-1, img.cols, CV_8UC1);
+		removeSeamX(img, newImg, SX);
+		img = newImg;
+	}
 	imshow("Lol", img);
 	waitKey(0);
 	destroyAllWindows();
@@ -198,7 +214,7 @@ void removeSeamX (Mat &img, Mat &newImg, seam &s) {
 		for (int j=0; j<initx; j++) {
 			newImg.at<uchar>(j, inity) = img.at<uchar>(j, inity);
 		}
-		for (int j=inity+1; j<img.cols; j++) {
+		for (int j=initx+1; j<img.cols; j++) {
 			newImg.at<uchar>(j-1, inity) = img.at<uchar>(j, inity);
 		}
 	}
